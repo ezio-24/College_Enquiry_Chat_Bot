54568fac96c3fd9344117a39643bb62bac030b15
Anand <108985014+ezio-24@users.noreply.github.com>
2023-08-09 20:36:52 +0530

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,24 @@
+function getBotResponse(input) {
+    // //rock paper scissors
+    // if (input == "rock") {
+    //     return "paper";
+    // } else if (input == "paper") {
+    //     return "scissors";
+    // } else if (input == "scissors") {
+    //     return "rock";
+    // }
+
+
+    if (input) {
+        return "We are currently offline. Please leave a message along with your contact details. Our support team will contact you shortly ! ";
+    }
+
+    // // Simple responses
+    // if (input == "Hi") {
+    //     return "hello! How Can i Help You?";
+    // } else if (input == "How Many Cources Available In This College") {
+    //     return "MCA , MSC , BCA , BSC, Ets..!";
+    // } else {
+    //     return "Any Other Queries About Cource Visit The College!";
+    // }
+}
